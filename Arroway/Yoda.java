917656d7967850395eb2db9bc1bd7b763dbe0306
Veress Máté <43212975+theefues@users.noreply.github.com>
2019-09-18 15:20:00 +0200

    Create Yoda.java

---
+++ Arroway/Yoda.java
@@ -0,0 +1,9 @@
+public class Yoda {
+    public static void main(String[] args) {
+      String text = null;
+      if ("Text".equals(text))
+      System.out.println("Text".equals(text));
+      if (text.equals("Text"))
+      System.out.println("Text".equals(text));
+    }
+}
