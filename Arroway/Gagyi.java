7d38fec4dcb49ac491eb87963439535f6485976d
Veress Máté <43212975+theefues@users.noreply.github.com>
2019-09-23 12:10:15 +0200

    Create Gagyi.java

---
+++ Arroway/Gagyi.java
@@ -0,0 +1,10 @@
+import java.lang.Number;
+class Gagyi {
+        public static void main (String[] args) {
+        int x = 300;
+        int t = 300;
+        while (x <= t && x >= t && t != x);
+        System.out.println("infinity");
+        }
+
+}
